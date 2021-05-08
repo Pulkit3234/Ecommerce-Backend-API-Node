437f0f5ef938cc05578d045903ea4972c5ad6599
Pulkit3234 <pulkitsh08@gmail.com>
2021-05-08 09:19:12 +0530

    changes

--- controllers/cart.js
+++ controllers/cart.js
@@ -3,50 +3,39 @@ const Order = require('../models/Order');
 
 exports.recieveCart = async (req, res, next) => {
 	console.log(req.body);
-	const { cartItems, totalPrice, totalItems, wishlist, shippingAddress, paymentMethod,} = req.body;
+	const { cartItems, totalPrice, totalItems, wishlist, shippingAddress, paymentMethod } = req.body;
 
 	const id = req.id;
-    console.log(id);
-    
-    try {
-       
-		if (existingOrder) {
-			res.json({ message: 'Order Exists', order: existingOrder });
-		} else {
-			console.log('else');
-			const order = new Order({
-				user: req.id,
-				shippingAddress,
-				cartItems,
-				totalPrice,
-                totalItems,
-                paymentMethod
-			});
+	console.log(id);
 
-			const orderCreated = await order.save();
-			res.status(200).json({ order: orderCreated });
-		}
+	try {
+		const order = new Order({
+			user: req.id,
+			shippingAddress,
+			cartItems,
+			totalPrice,
+			totalItems,
+			paymentMethod,
+		});
+
+		const orderCreated = await order.save();
+		res.status(200).json({ order: orderCreated });
 	} catch (error) {
 		console.log(error);
 		res.json({ error });
 	}
 };
 
-
 exports.orderStatus = async (req, res, next) => {
-    const { orderId: id, isPaid } = req.body;
-    try {
-
-        const orderUpdated = await findByIdAndUpdate(orderId, { isPaid });
-        res.status(200).json({ order: orderUpdated });
-
-
-        
-        
-    } catch (error) {
-        console.log(error);
-        res.status(401).json({ message: error });
-    };
+	const { orderId: id, isPaid } = req.body;
+	console.log(isPaid);
+	try {
+		const orderUpdated = await Order.findByIdAndUpdate(id, { isPaid }, { new: true });
+		res.status(200).json({ order: orderUpdated });
+	} catch (error) {
+		console.log(error);
+		res.status(401).json({ message: error });
+	}
 };
 
 exports.sendCart = async (req, res, next) => {
